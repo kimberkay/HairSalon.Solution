113fc5fcfad246948a00c8e36387eecb9b58ca3f
Kim Brannian <brannian@gmail.com>
2021-12-29 09:37:59 -0800

    add client model

---
+++ HairSalon/Models/Clients.cs
@@ -0,0 +1,11 @@
+namespace HairSalon.Models
+{
+  public class Client
+  {
+    public int ClientId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+
+    public string HairColorFormula { get; set; }
+  }
+}
